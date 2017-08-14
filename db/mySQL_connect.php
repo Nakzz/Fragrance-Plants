9638e52629e45e2fed1d6f8e62f6e945452dfd1b
Nakzz <naqibprio10@gmail.com>
2017-08-14 12:09:15 -0400

    Database file starting

---
+++ db/mySQL_connect.php
@@ -0,0 +1,11 @@
+<?php
+  $db_file_exists= false;
+  bool file_exists ( string dbinfo_local.php )
+
+  if ()$db_file_exists=true){
+    echo "File Found.";
+  }
+  require_once(dbinfo_local.php);
+  //$db_connect=
+
+ ?>
