8300fbe398bf6976b2cb0b3d80caaaad64e3dd53
nakzz <naqibprio10@gmail.com>
2017-08-30 16:58:39 -0700

    Fixed contact page grayscale problem

--- contact.php
+++ contact.php
@@ -206,22 +206,23 @@ include_once("db/config.php");?>
     <!-- endinject -->
 
     <script>
-        $(document).ready(function(){
-           $("#grey-map").gMap({
-               markers: [
-                   {
-                       latitude: 40.746734,
-                       longitude: -73.992144,
-                       html: '<div class="test_marker"> <a href="https://goo.gl/maps/hpxY3rzupdr">Fragrance Plants & Flowers Inc.</a></div>'
+    $(document).ready(function() {
+        $("#grey-map").gMap({
+            markers: [{
+                latitude: 40.746734,
+                longitude: -73.992144,
+                html: '<div class="test_marker"> <a href="https://goo.gl/maps/hpxY3rzupdr">Fragrance Plants & Flowers Inc.</a></div>'
 
-                   }
-               ],
-               zoom: 16,
-               scrollwheel:false,
-               styles: [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}]
-           })
-        });
-    </script>
+            }],
+            zoom: 16,
+            scrollwheel: false,
+
+
+                }]
+
+        })
+    });
+</script>
   </body>
 
   </html>
